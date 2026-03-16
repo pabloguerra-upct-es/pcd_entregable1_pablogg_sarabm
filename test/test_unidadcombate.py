37841aacb1e6138fa5664c1e3b56268c7516f1d5
sarabayona-upct-es <sara.bayona@edu.upct.es>
2026-03-16 12:11:42 +0100

    Test de Unidda de Combate

---
+++ test/test_unidadcombate.py
@@ -0,0 +1,33 @@
+import pytest
+from Imperio_Galactico import UnidadCombate, AccesoDenegado
+
+''' Como UnidadCombate es una clase abstracta, no podemos crearla directamente.
+    Creamos una clase hija mínima solo para poder testear la lógica del padre.'''
+
+class DummyUnidadCombate(UnidadCombate):
+    def transmitirMensaje(self, mensaje: str) -> None:
+        pass 
+
+def test_creacion_unidad_correcta():
+    """Verifica que una unidad se crea con datos válidos."""
+    unidad = DummyUnidadCombate("TIE-01", 1234)
+    assert unidad._idCombate == "TIE-01"
+    assert unidad._claveCifrada == 1234
+
+def test_error_tipo_id_combate():
+    """Verifica que lanza TypeError si el ID no es un string."""
+    with pytest.raises(TypeError):
+        DummyUnidadCombate(1234, 1234) # Enviamos un int en lugar de str
+
+def test_autenticacion_exitosa():
+    """Verifica que devuelve True si la clave coincide."""
+    unidad = DummyUnidadCombate("DS-01", 9999)
+    assert unidad.autentificarClave(9999) is True
+
+def test_excepcion_acceso_denegado():
+    """Verifica que lanza nuestra excepción personalizada si la clave falla."""
+    unidad = DummyUnidadCombate("DS-01", 9999)
+    
+    # Solo comprobamos que la excepción ocurre al meter la clave mal
+    with pytest.raises(AccesoDenegado):
+        unidad.autentificarClave(1111)
