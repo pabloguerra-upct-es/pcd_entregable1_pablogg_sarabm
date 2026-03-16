b929deb8c8ecea2a7932981afcc9c08bee3ba42c
pabloguerra-upct-es <pablo.guerra@edu.upct.es>
2026-03-16 11:49:38 +0100

    Añadidas las funciones de la clase UnidadCombate

--- Imperio_Galactico.py
+++ Imperio_Galactico.py
@@ -1,4 +1,5 @@
 from enum import Enum
+from abc import ABCMeta, abstractmethod
 
 class EUbicacion(Enum):
     ENDOR = "Endor"
@@ -10,12 +11,36 @@ class EClaseNave(Enum):
     ECLIPSE = "Eclipse"
     SOBERANO = "Soberano"
 
-class UnidadCombate:
+class AccesoDenegado(Exception):
+    pass
+
+class UnidadCombate(metaclass=ABCMeta):
     def __init__(self, idCombate:str, claveCifrada:int):
+        if not isinstance(idCombate, str):
+            raise TypeError("idCombate TIENE QUE SER STR")
+        
+        if not isinstance(claveCifrada, int):
+            raise TypeError("claveCifrada TIENE QUE SER INT")
+
         self._idCombate = idCombate
         self._claveCifrada = claveCifrada
 
-class Nave(UnidadCombate):
+    def autentificarClave(self, clave_a_verificar:int)->bool:
+        if not isinstance(clave_a_verificar, int):
+            raise TypeError("clave_a_verificar TIENE QUE SER INT")
+        
+        if clave_a_verificar == self._claveCifrada:
+            print(f'CLAVE CORRECTA.\nUnidad de Combate {self._claveCifrada} autentificada correctamente')
+            return True
+        raise AccesoDenegado('CLAVE INCORRECTA.\nRevise la clave introducida')
+
+    @abstractmethod
+    def transmitirMensaje(self, mensaje:str)->None:
+        if not isinstance(mensaje, str):
+            raise TypeError("mensaje TIENE QUE SER STR")
+        pass
+
+class Nave(UnidadCombate, metaclass=ABCMeta):
     def __init__(self, idCombate, claveCifrada, nombre:str, catalogoPiezas:list[str]):
         super().__init__(idCombate, claveCifrada)
         self._nombre = nombre
@@ -40,7 +65,7 @@ class CazaEstelar(Nave):
         super().__init__(idCombate, claveCifrada, nombre, catalogoPiezas)
         self.__dotacion = dotacion
 
-class Usuario:
+class Usuario(metaclass=ABCMeta):
     def __init__(self, idUsuario:str, nombre:str):
         self._idUsuario = idUsuario
         self._nombre = nombre
