dc5ad40a140dd9ce7a9c4b5c30e75156543946b6
pabloguerra-upct-es <pablo.guerra@edu.upct.es>
2026-03-18 12:03:16 +0100

    Añadiendo la clase Repuesto completa

--- Imperio_Galactico.py
+++ Imperio_Galactico.py
@@ -43,6 +43,43 @@ class UnidadCombate(metaclass=ABCMeta):
             raise TypeError("mensaje TIENE QUE SER STR")
         pass
 
+class Repuesto:
+    def __init__(self, nombre:str, proveedor:str, cantidad:int, precio:float):
+        if not isinstance(nombre, str):
+            raise TypeError("nombre TIENE QUE SER str")
+        
+        if not isinstance(proveedor, str):
+            raise TypeError("proveedor TIENE QUE SER str")
+        
+        if not isinstance(cantidad, int):
+            raise TypeError("cantidad TIENE QUE SER int")
+        
+        if not isinstance(precio, (int, float)):
+            raise TypeError("precio TIENE QUE SER float")
+        
+        self.nombre = nombre
+        self.proveedor = proveedor
+        self.__cantidad = cantidad
+        self.precio = precio
+    
+    def obtenerUnidades(self):
+        return self.__cantidad
+    
+    def aumentarStock(self, cantidad_aumentar:int):
+        if not isinstance(cantidad_aumentar, int):
+            raise TypeError("cantidad_aumentar TIENE QUE SER int")
+        
+        self.__cantidad += cantidad_aumentar
+
+    def reducirStock(self, cantidad_disminuir:int):
+        if not isinstance(cantidad_disminuir, int):
+            raise TypeError("cantidad_disminuir TIENE QUE SER int")
+        
+        if cantidad_disminuir > self.__cantidad:
+            raise ValueError("La cantidad a disminuir es mayor que la cantidad actual en stock")
+    
+        self.__cantidad -= cantidad_disminuir
+
 class Nave(UnidadCombate, metaclass=ABCMeta):
     def __init__(self, idCombate, claveCifrada, nombre:str, catalogoPiezas:list[str]):
         super().__init__(idCombate, claveCifrada)
@@ -52,7 +89,7 @@ class Nave(UnidadCombate, metaclass=ABCMeta):
     @abstractmethod
     def transmitirMensaje(self, mensaje: str):
         pass
-    
+
     def consultarCatalogo(self):
         if len(self._catalogoPiezas) == 0:
             raise EmptyError("El catalogo esta vacio")
@@ -144,13 +181,6 @@ class Comandante(Usuario):
 class OperarioAlmacen(Usuario):
     def __init__(self, idUsuario, nombre):
         super().__init__(idUsuario, nombre)
-
-class Repuesto:
-    def __init__(self, nombre:str, proveedor:str, cantidad:int, precio:float):
-        self.nombre = nombre
-        self.proveedor = proveedor
-        self.__cantidad = cantidad
-        self.precio = precio
 
 class Almacen:
     def __init__(self, nombre:str, ubicacion:str, catalogoRepuestos:list[Repuesto]):
